e54437c5795f3911315fefb98e929d68a5199a8c
Renato T M Lima <renato.matura@gmail.com>
2025-03-17 21:50:49 -0400

    added biome and ran check

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,22 +1,23 @@
 import type { ComponentProps } from 'react'
-import { tv, VariantProps } from 'tailwind-variants'
+import { type VariantProps, tv } from 'tailwind-variants'
 
 const buttonVariants = tv({
-    base: 'text-zinc-400 rounded-lg hover:text-zinc-100 hover:bg-zinc-800 disabled:opacity-50 disabled:pointer-events-none',
-    variants: {
-        size: {
-            default: 'px-3 py-2',
-            icon: 'p-2',
-            'icon-sm': 'p-1'
-        }
+  base: 'text-zinc-400 rounded-lg hover:text-zinc-100 hover:bg-zinc-800 disabled:opacity-50 disabled:pointer-events-none',
+  variants: {
+    size: {
+      default: 'px-3 py-2',
+      icon: 'p-2',
+      'icon-sm': 'p-1',
     },
-    defaultVariants: {
-        size: 'default'
-    }
+  },
+  defaultVariants: {
+    size: 'default',
+  },
 })
 
-type ButtonProps = ComponentProps<'button'> & VariantProps<typeof buttonVariants>
+type ButtonProps = ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants>
 
-export function Button({size, className, ...props}: ButtonProps) {
-    return <button className={buttonVariants({ size, className })} {...props}/>
+export function Button({ size, className, ...props }: ButtonProps) {
+  return <button className={buttonVariants({ size, className })} {...props} />
 }
--- src/components/upload-widget/index.tsx
+++ src/components/upload-widget/index.tsx
@@ -1,30 +1,30 @@
-import * as Collapsible from "@radix-ui/react-collapsible";
-import { UploadWidgetDropzone } from "./drozone";
-import { UploadWidgetHeader } from "./header";
-import { UploadWidgetUploadList } from "./upload-list";
-import { useState } from "react";
-import { UploadWidgetMinimizedButton } from "./minimized-button";
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { useState } from 'react'
+import { UploadWidgetDropzone } from './drozone'
+import { UploadWidgetHeader } from './header'
+import { UploadWidgetMinimizedButton } from './minimized-button'
+import { UploadWidgetUploadList } from './upload-list'
 
-export function UploadWidget(){
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-    
-    return (
-        <Collapsible.Root onOpenChange={setIsWidgetOpen}>
-            <div className="bg-zinc-900 w-[360px] rounded-xl shadow-shape overflow-hidden">
-                {!isWidgetOpen && <UploadWidgetMinimizedButton />}
-                
-                <Collapsible.Content>
-                    <UploadWidgetHeader/>
+export function UploadWidget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
 
-                    <div className="flex flex-col gap-4 py-3">
-                        <UploadWidgetDropzone />
+  return (
+    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+      <div className="bg-zinc-900 w-[360px] rounded-xl shadow-shape overflow-hidden">
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
-                        <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+        <Collapsible.Content>
+          <UploadWidgetHeader />
 
-                        <UploadWidgetUploadList />
-                    </div>
-                </Collapsible.Content>
-            </div>
-        </Collapsible.Root>
-    )
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
+
+            <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+
+            <UploadWidgetUploadList />
+          </div>
+        </Collapsible.Content>
+      </div>
+    </Collapsible.Root>
+  )
 }
