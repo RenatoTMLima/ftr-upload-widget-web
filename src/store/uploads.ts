eaf56bb8d998f27cbee4d6b337678e0e7bd02202
Renato T M Lima <renato.matura@gmail.com>
2025-03-21 00:35:55 -0400

    Added zustand, created a store

---
+++ src/store/uploads.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+
+export type Upload = {
+    name: string
+    file: File
+}
+
+type UploadState = {
+    uploads: Map<string, Upload>
+    addUploads: (files: File[]) => void
+}
+
+export const useUploads = create<UploadState>((set, get) => {
+    function addUploads(files: File[]) {
+        for(const file of files) {
+            const uploadId = crypto.randomUUID()
+
+            const upload = {
+                name: file.name,
+                file
+            } satisfies Upload
+
+            set(state => {
+                return {
+                    uploads: state.uploads.set(uploadId, upload)
+                }
+            })
+        }
+    }
+    
+    return {
+        uploads: new Map(),
+        addUploads
+    }
+})
