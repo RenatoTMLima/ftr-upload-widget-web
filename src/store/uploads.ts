cba0f898e8175705991ffee6c19858ade13990f9
Renato T M Lima <renato.matura@gmail.com>
2025-03-21 00:42:29 -0400

    format and lint ts files

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,35 +1,35 @@
-import { create } from "zustand";
+import { create } from 'zustand'
 
 export type Upload = {
-    name: string
-    file: File
+  name: string
+  file: File
 }
 
 type UploadState = {
-    uploads: Map<string, Upload>
-    addUploads: (files: File[]) => void
+  uploads: Map<string, Upload>
+  addUploads: (files: File[]) => void
 }
 
 export const useUploads = create<UploadState>((set, get) => {
-    function addUploads(files: File[]) {
-        for(const file of files) {
-            const uploadId = crypto.randomUUID()
+  function addUploads(files: File[]) {
+    for (const file of files) {
+      const uploadId = crypto.randomUUID()
 
-            const upload = {
-                name: file.name,
-                file
-            } satisfies Upload
+      const upload = {
+        name: file.name,
+        file,
+      } satisfies Upload
 
-            set(state => {
-                return {
-                    uploads: state.uploads.set(uploadId, upload)
-                }
-            })
+      set(state => {
+        return {
+          uploads: state.uploads.set(uploadId, upload),
         }
+      })
     }
-    
-    return {
-        uploads: new Map(),
-        addUploads
-    }
+  }
+
+  return {
+    uploads: new Map(),
+    addUploads,
+  }
 })
