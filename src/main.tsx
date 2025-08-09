468e21b978d16c5a0899783f190d0219e32fa294
Renato Takao Matura de Lima <renato.matura@gmail.com>
2025-08-08 22:03:49 -0400

    Something

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './app.tsx'
 
+// something
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
